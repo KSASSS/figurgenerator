96bc507fe0b5122c9bc1a4b0627fc581f9dc3fe6
Axel <utv-react@ks.no>
2019-07-01 14:51:52 +0200

    Lagt til skjelett

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+export default class Sidebar extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+      return (
+        1
+      )
+    }
+}
