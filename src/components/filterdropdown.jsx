416681024e3af0a7c78c646fca95ac03ddbc8452
Axel <utv-react@ks.no>
2019-07-01 14:06:27 +0200

    Lagt til skjelett

--- src/components/filterdropdown.jsx
+++ src/components/filterdropdown.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
-export default function FilterDropDown() {
+/* Dropdown imports */
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
+class FilterDropdown extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        
+    }
 }
