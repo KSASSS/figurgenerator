4d756c94307197c600aacc16c8ab3b17c2c11dd7
Axel <utv-react@ks.no>
2019-07-03 15:54:58 +0200

    Endret chosenfiltergroup til å være en dropdown

--- src/components/ChosenFilterbox/ChosenFiltergroup.jsx
+++ src/components/ChosenFilterbox/ChosenFiltergroup.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 import Typography from "@material-ui/core/Typography";
 
+/* Dropdown imports */
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 export default class ChosenFiltergroup extends React.Component {
     constructor(props) {
         super(props);
@@ -14,8 +20,18 @@ export default class ChosenFiltergroup extends React.Component {
     render() {
         return(
             <div>
-                <Typography>{this.props.title}</Typography>
-                <p>{this.props.groups}</p>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                        <Typography>{this.props.title}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <p>{this.props.groups}</p>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
             </div>
         );
     }
